a686ba34aa817eeb6368b664d43beb9e24103d11
Maharshi Shah <maharshishah@mail.usf.edu>
2020-11-16 00:05:26 +0530

    Added loading image animation and logo for landing page

--- app/src/main/java/com/maharshiappdev/pseudogen/MainActivity.java
+++ app/src/main/java/com/maharshiappdev/pseudogen/MainActivity.java
@@ -2,12 +2,29 @@ package com.maharshiappdev.pseudogen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView loadingImageView;
+    public void startLoadingAnimation()
+    {
+        loadingImageView.animate().rotation(7200).setDuration(5000);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadingImageView = findViewById(R.id.loadingImageView);
+        startLoadingAnimation();
+
+        /*Check Sign-In STATE
+          If signed in, move to CodeEditor Activity
+          Else Move to Log-in Activity
+        */
+/*        Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+        startActivity(intent);*/
     }
 }
