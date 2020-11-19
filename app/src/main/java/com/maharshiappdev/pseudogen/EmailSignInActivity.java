e380e402a4ece09e455a489fdda9c5843858e777
unknown <maharshishah@mail.usf.edu>
2020-11-19 14:01:48 +0530

    Integrated FirebaseDatabase and uploading user credential data to cloud database

--- app/src/main/java/com/maharshiappdev/pseudogen/EmailSignInActivity.java
+++ app/src/main/java/com/maharshiappdev/pseudogen/EmailSignInActivity.java
@@ -18,11 +18,18 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class EmailSignInActivity extends AppCompatActivity {
+    private final FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private final DatabaseReference firebaseDatabaseRef = FirebaseDatabase.getInstance().getReference();
     EditText usernameEditText;
     EditText passwordEditText;
-    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    String userEmail;
+    String userPassword;
+    String firstName;
+    String lastName;
 
     public void signInClicked(View view)
     {
@@ -41,6 +48,32 @@ public class EmailSignInActivity extends AppCompatActivity {
         passwordEditText = findViewById(R.id.passwordEditText);
     }
 
+    public void signInUser(String username, String password)
+    {
+        mAuth.signInWithEmailAndPassword(username, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful())
+                        {
+                            Log.i("INFO:", "User signed in " + username);
+                            //TODO Switch Activity
+                        }else
+                        {
+                            try
+                            {
+                                throw task.getException();
+                            }catch (Exception e)
+                            {
+                                Toast.makeText(EmailSignInActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+
+                            startSignUpDialog();
+                        }
+                    }
+                });
+    }
+
     public void createNewUser(String username, String password)
     {
         mAuth.createUserWithEmailAndPassword(username, password)
@@ -50,6 +83,9 @@ public class EmailSignInActivity extends AppCompatActivity {
                      if(task.isSuccessful())
                      {
                          Toast.makeText(getApplicationContext(), "Signed Up!", Toast.LENGTH_SHORT).show();
+                         //Update Database
+                         writeToDatabase(firstName, lastName, userEmail, userPassword);
+
                          //TODO Switch Activity
                      }else
                      {
@@ -64,32 +100,6 @@ public class EmailSignInActivity extends AppCompatActivity {
                 });
     }
 
-    public void signInUser(String username, String password)
-    {
-        mAuth.signInWithEmailAndPassword(username, password)
-        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful())
-                {
-                    Log.i("INFO:", "User created " + username);
-                    //TODO Switch Activity
-                }else
-                {
-                    try
-                    {
-                        throw task.getException();
-                    }catch (Exception e)
-                    {
-                        Toast.makeText(EmailSignInActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-
-                    startSignUpDialog();
-                }
-            }
-        });
-    }
-
     public void startSignUpDialog()
     {
 
@@ -100,6 +110,9 @@ public class EmailSignInActivity extends AppCompatActivity {
 
         final EditText signUpEmailText = dialogView.findViewById(R.id.signUpEmailText);
         final EditText signUpPasswordEditText = dialogView.findViewById(R.id.signUpPasswordEditText);
+        final EditText firstNameEditText = dialogView.findViewById(R.id.firstNameEditText);
+        final EditText lastNameEditText = dialogView.findViewById(R.id.lastNameEditText);
+
         final EditText retypePasswordEditText = dialogView.findViewById(R.id.retypePasswordEditText);
 
         alertDialogBuilder.setView(dialogView)
@@ -109,8 +122,11 @@ public class EmailSignInActivity extends AppCompatActivity {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         //TODO: Create new user from details entered in alert dialog
-                        String userEmail = signUpEmailText.getText().toString();
-                        String userPassword = signUpPasswordEditText.getText().toString();
+                        userEmail = signUpEmailText.getText().toString();
+                        userPassword = signUpPasswordEditText.getText().toString();
+                        firstName = firstNameEditText.getText().toString();
+                        lastName = lastNameEditText.getText().toString();
+
                         String retypePassword = retypePasswordEditText.getText().toString();
                         if(!userPassword.isEmpty() && !retypePassword.isEmpty())
                         {
@@ -124,5 +140,13 @@ public class EmailSignInActivity extends AppCompatActivity {
                         dialog.cancel();
                     }
                 }).show();
+    }
+
+    public void writeToDatabase(String firstName, String lastName, String userEmail, String userPassword)
+    {
+        firebaseDatabaseRef.child("Users").child("First Name").setValue(firstName);
+        firebaseDatabaseRef.child("Users").child("Last Name").setValue(lastName);
+        firebaseDatabaseRef.child("Users").child("Email").setValue(userEmail);
+        firebaseDatabaseRef.child("Users").child("Password").setValue(userPassword);
     }
 }
