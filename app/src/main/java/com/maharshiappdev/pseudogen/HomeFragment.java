a1e1d5567e216b978e37846aa34abf838760d814
unknown <maharshishah@mail.usf.edu>
2020-11-28 13:14:16 +0530

    Deleted dead activities and images

--- app/src/main/java/com/maharshiappdev/pseudogen/HomeFragment.java
+++ app/src/main/java/com/maharshiappdev/pseudogen/HomeFragment.java
@@ -1,64 +1,92 @@
 package com.maharshiappdev.pseudogen;
-
 import android.os.Bundle;
-
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ExpandableListView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class HomeFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public HomeFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+public class HomeFragment extends Fragment implements AdapterView.OnItemClickListener{
+    View view;
+    ExpandableListAdapter listAdapter;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ExpandableListView expandableListView = view.findViewById(R.id.homeExpandableListView);
+        prepareListData();
+
+        listAdapter = new ExpandableListAdapter(getActivity().getApplicationContext(), listDataHeader, listDataChild);
+        // setting list adapter
+        expandableListView.setAdapter(listAdapter);
+
+        //set to expand for each group in expandableListView
+        for(int i = 0; i < listAdapter.getGroupCount(); i++ )
+        {
+            expandableListView.expandGroup(i);
+        }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+    }
+
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        // Adding child data
+        listDataHeader.add("Intro to Pseudocode");
+        listDataHeader.add("Data Structures");
+        listDataHeader.add("Analysis of Algorithms");
+
+        // Adding child data
+        List<String> introToPseudocodeHeader = new ArrayList<String>();
+        introToPseudocodeHeader.add("Motivation & Insight");
+        introToPseudocodeHeader.add("Syntax Used");
+        introToPseudocodeHeader.add("Examples");
+
+        List<String> dataStructuresHeader = new ArrayList<String>();
+        dataStructuresHeader.add("Arrays");
+        dataStructuresHeader.add("Strings");
+        dataStructuresHeader.add("Linked Lists");
+        dataStructuresHeader.add("Stack");
+        dataStructuresHeader.add("Queue");
+        dataStructuresHeader.add("Set");
+        dataStructuresHeader.add("Map");
+        dataStructuresHeader.add("Hash Tables");
+        dataStructuresHeader.add("Priority Queue");
+        dataStructuresHeader.add("Heaps");
+        dataStructuresHeader.add("Binary Tree");
+        dataStructuresHeader.add("Binary Search Tree");
+        dataStructuresHeader.add("Graphs");
+
+        List<String> analysisOfAlgorithmsHeader = new ArrayList<String>();
+        analysisOfAlgorithmsHeader.add("Time & Space Analysis");
+        analysisOfAlgorithmsHeader.add("Designing Algorithms");
+        analysisOfAlgorithmsHeader.add("Divide & Conquer");
+        analysisOfAlgorithmsHeader.add("Greedy Algorithms");
+        analysisOfAlgorithmsHeader.add("Dynamic Programming");
+        analysisOfAlgorithmsHeader.add("Searching & Sorting");
+
+        listDataChild.put(listDataHeader.get(0), introToPseudocodeHeader); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), dataStructuresHeader);
+        listDataChild.put(listDataHeader.get(2), analysisOfAlgorithmsHeader);
     }
 }
